808bcabded200c1dde362ffe8c6456ea2700b6ea
NightBlue06 <yasin.omeroglu.2007@gmail.com>
2023-03-04 13:06:57 +0300

    Değişkenler ve veri tipleri dersi girişi yapıldı
    X_X

---
+++ 01_degiskenler_ve_veri_tipleri.py
@@ -0,0 +1,24 @@
+# Değişken tanımlama örnekleri
+x = 5
+# Not: sağdaki değer herzaman sola aktarılır
+y = "Merhaba, Yasin X_X"
+z = True
+
+# VERİ TİPLERİ
+# 1. Tam sayılar (integer)
+x = 15
+y = -15
+z = 0
+
+# 2.  Ondalıklı sayılar (floats)
+
+x = 3.14
+y = -2.25
+z = 1.0
+
+# 3. Metin (string)
+
+selam = "Neden ölmedin"
+ad = "Yasin"
+mesaj = selam + " " + ad
+print(mesaj)
